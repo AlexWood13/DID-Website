983b166d3c77a06b848a97b45d63c27d4f8a4420
AlexWood13 <32333277+AlexWood13@users.noreply.github.com>
2018-11-25 23:30:35 +0000

    added error handlers and data return php

--- contactform.php
+++ contactform.php
@@ -1,6 +1,6 @@
 <?php
 
-// Checks if form has been submitted
+// Checks if reCAPTCHA has been submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     function post_captcha($user_response) {
         $fields_string = '';
@@ -23,24 +23,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Call the function post_captcha
     $res = post_captcha($_POST['g-recaptcha-response']);
     if (!$res['success']) {
-      header("Location: ../index.php?error=reCAPTCHAfail");
+      header("Location: index.php?error=reCAPTCHAfail");
       exit();
     } else {
 
+//checks if form  has been submitted
 if (isset($_POST['submit'])) {
+
+  //grabbing data from form
   $name = $_POST['name'];
   $subject = $_POST['subject'];
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailTo = "ajwood1998@depffleague.co.uk";
-  $headers = "From: ".$mailFrom;
-  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+  //Error handlers
 
-  mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.php?mailsent");
-}
+  //checking for empty inputs
+  if (empty($name) || empty($subject) || empty($mailFrom) || empty($message)) {
+    header("Location: index.php?error=emptyfields&name=".$name."&mail=".$mailFrom);
+    exit();
+  }
+
+  //checking for an invalid e-mail.
+  else if (!filter_var($mailFrom, FILTER_VALIDATE_EMAIL)) {
+      header("Location: index.php?error=invalidmail&uid=".$name);
+      exit();
+  }
+
+  else {
+    //setting data to variables
+    $mailTo = "ajwood1998@depffleague.co.uk";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    //sending data to email with text formatted
+    mail($mailTo, $subject, $txt, $headers);
+
+    //reloading the page once completed and displaying email success message
+    header("Location: index.php?mailsent=successful");
+    exit;
+  }
+
     }
+  }
 }
  ?>
-*/
--- index.php
+++ index.php
@@ -1,33 +1,55 @@
 
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <link rel="stylesheet" href="style.css" type="text/css">
-      <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src='https://www.google.com/recaptcha/api.js'></script>    
-    </head>
-      <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="style.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+  <body>
 
-    <!-- Navbar -->
-    <div class="header-nav">
-      <a href="#about">About</a>
-      <a href="#download">Download</a>
-      <a href="#game">Game</a>
-      <a href="#contact">Contact</a>
-    </div>
+<!-- Navbar -->
+<div class="header-nav">
+  <a href="#about">About</a>
+  <a href="#download">Download</a>
+  <a href="#game">Game</a>
+  <a href="#contact">Contact</a>
+</div>
 
-    <!-- Header + start of main-wrapper -->
-    <div class="main-wrapper">
-      <div class="wrapper">
-      <h1 id="about"> Welcome to DepffThrone </h1>
-      </div>
+<!-- Header + start of main-wrapper -->
+<div class="main-wrapper">
+  <div class="wrapper">
+  <h1 id="about"> Welcome to DepffThrone </h1>
+  <?php
+     if (isset($_GET['error'])) {
+       //empty filed error message
+       if ($_GET['error'] == "emptyfields") {
+         echo '<p class="error"> Fill in all fields! </p>';
+       }
+       //invalid email error message
+       else if ($_GET['error'] == "invalidmail"){
+         echo '<p class="error">Invalid e-mail!</p>';
+       }
+       //reCAPTCHA error message
+       else if ($_GET['error'] == "reCAPTCHAfail"){
+         echo '<p class="error">You have not validated the reCAPTCHA. Please try again!</p>';
+       }
+     }
+     // create a success message if message was sent
+       else if (isset($_GET["mailsent"])) {
+         if ($_GET["mailsent"] == "successful") {
+           echo '<p class="mailsentsuccess">Email sent successfully!</p>';
+       }
+     }
+   ?>
+  </div>
 
 <!-- Blurred image -->
-<div class="wrapper">
+  <div class="wrapper">
   <!--  "https://warriors.fandom.com/ru/wiki/%D0%A4%D0%B0%D0%B9%D0%BB:697d27e61a951bcce39eb4ced2ec9f3a.jpg" alt="image of the Lich King from Warcraft 3 The Frozen Throne" title="Link to Source"-->
     <section class="showcase" alt="Image of the Lich King from Warcraft 3 The Frozen Throne">
       <div class="content">
@@ -37,85 +59,103 @@
           </div>
       </div>
     </section>
-</div>
-
-  <!-- Services grid -->
-    <section class="services">
-      <div class="grid-container grid-3">
-        <div>
-          <i class="fas fa-desktop fa-3x"></i>
-          <h3>About</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod.</p>
-        </div>
-        <div>
-          <i class="fas fa-chalkboard-teacher fa-3x"></i>
-          <h3>Guides</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod.</p>
-        </div>
-        <div>
-          <i class="fas fa-flag-checkered fa-3x"></i>
-          <h3>TypeRacer</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod.</p>
-        </div>
-      </div>
-    </section>
   </div>
+
+<!-- Services grid -->
+  <section class="services">
+    <div class="grid-container grid-3">
+      <div>
+        <i class="fas fa-desktop fa-3x"></i>
+        <h3>About</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod.</p>
+      </div>
+      <div>
+        <i class="fas fa-chalkboard-teacher fa-3x"></i>
+        <h3>Guides</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod.</p>
+      </div>
+      <div>
+        <i class="fas fa-flag-checkered fa-3x"></i>
+        <h3>TypeRacer</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod.</p>
+      </div>
+    </div>
+  </section>
+</div>
 
 <!-- new main to fix gridding issue -->
 <div class="main-wrapper-2">
-  <div class="wrapper">
-    <!-- Keyboard image with download -->
-    <div class="keyboard-text">
-      <h2 id="download"> Hotkeys! </h2>
-      <p> These are the hotkeys that you will most likely be using when playing Warcraft 3, The Frozen Thone. Use the typeracer game below to test you typing speed, so you can be as fast as possible when you use the hotkeys</p>
-        
+<div class="wrapper">
+  <!-- Keyboard image with download -->
+  <div class="keyboard-text">
+    <h2 id="download"> Hotkeys! </h2>
+    <p> These are the hotkeys that you will most likely be using when playing Warcraft 3, The Frozen Thone. Use the typeracer game below to test you typing speed, so you can be as fast as possible when you use the hotkeys</p>
+
+  </div>
+  <div class="keyboard-image">
+    <img src="assets/keyboard.jpg" alt="Image of a keyboard">
+  </div>
+</div>
+
+<!-- Type Racer Game -->
+<div class="wrapper">
+  <div class="typeracer">
+    <!-- Instructions -->
+    <div class="instructions">
+      <h5>Instructions</h5>
+      <p id="game">Type each word in the given amount of seconds to score. To play again, just type the current word. Your score will reset. Please ensure that you type in lowercase letters when playing the game.</p>
     </div>
-    <div class="keyboard-image">
-      <img src="assets/keyboard.jpg" alt="Image of a keyboard">
+
+    <!-- Word & Input -->
+    <div class="input">
+      <p>Type The Given Word Within <span id="seconds"> </span> Seconds:</p>
+      <h2 id="current-word"></h2>
+      <input type="text" placeholder="Start typing..." id="word-input">
+      <h4 id="message"></h4>
+
+      <!-- Time & Score Columns -->
+      <h3>Time Left:
+        <span id="time">0</span>
+      </h3>
+      <h3>Score:
+        <span id="score">0</span>
+      </h3>
     </div>
   </div>
-    
-    
+</div>
 
-
-  <!-- Type Racer Game -->
-  <div class="wrapper">
-    <div class="typeracer">
-      <!-- Instructions -->
-      <div class="instructions">
-        <h5>Instructions</h5>
-        <p id="game">Type each word in the given amount of seconds to score. To play again, just type the current word. Your score will reset. Please ensure that you type in lowercase letters when playing the game.</p>
-      </div>
-
-      <!-- Word & Input -->
-      <div class="input">
-        <p>Type The Given Word Within <span id="seconds"> </span> Seconds:</p>
-        <h2 id="current-word"></h2>
-        <input type="text" placeholder="Start typing..." id="word-input">
-        <h4 id="message"></h4>
-
-        <!-- Time & Score Columns -->
-        <h3>Time Left:
-          <span id="time">0</span>
-        </h3>
-        <h3>Score:
-          <span id="score">0</span>
-        </h3>
-      </div>
-    </div>
-  </div>
-
+<!-- float clear -->
 <div class="clr">
 </div>
 
+<!-- contact form -->
 <div class="wrapper">
   <h2 class="contact-h2">Contact Us </h2>
   <form class="contact-form" action="contactform.php" method="post">
-    <input type="text" name="name" placeholder="Full name">
-    <input type="text" name="mail" placeholder="Your E-mail">
+    <?php
+    // Check if the user already tried submitting data.
+    // If they have, return that data.
+
+    // checking username.
+    if (!empty($_GET["name"])) {
+      echo '<input type="text" name="name" placeholder="Full Name" value="'.$_GET["name"].'">';
+    }
+    else {
+      echo '<input type="text" name="name" placeholder="Full Name">';
+    }
+
+    // checking e-mail.
+    if (!empty($_GET["mail"])) {
+      echo '<input type="text" name="mail" placeholder="E-mail" value="'.$_GET["mail"].'">';
+    }
+    else {
+      echo '<input type="text" name="mail" placeholder="E-mail">';
+    }
+    ?>
+
     <input type="text" name="subject" placeholder="Subject">
     <textarea name="message" rows="8" cols="80"     placeholder="Message"></textarea>
     <button type="submit" name="submit"> Send Mail </button>
